0c769b8d5193545a56f6d47c1bdb1e5ed498721c
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-06-03 21:59:13 +0800

    9.4 Express

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-const x = 1
+import express from 'express'
+
+const app = express()
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!').end()
+})
+
+app.listen(3003, () => {
+  console.log(`Server running on port 3003`)
+})
